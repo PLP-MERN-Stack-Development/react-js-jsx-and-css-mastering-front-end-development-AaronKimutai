cb039ff1f5689eb1c9065c5cf8d406ffa5a1fb5e
AaronKimutai <aaronkimutai24@gmail.com>
2025-10-23 00:50:25 +0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+const repoName = 'react-js-jsx-and-css-masterinf-front-end-development';
+
 export default defineConfig({
   plugins: [
     react(),
-    
     tailwindcss(), 
   ],
+  
+  base: `/${repoName}/`, 
 })
