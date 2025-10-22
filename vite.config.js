56ece9ee4e5da75726472029e81c4179e90a0ab7
AaronKimutai <aaronkimutai24@gmail.com>
2025-10-23 01:22:20 +0300

    Solved some issues

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-const repoName = 'react-js-jsx-and-css-mastering-front-end-development';
+const repoName = 'react-js-jsx-and-css-mastering-front-end-development-AaronKimutai';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), 
+    tailwindcss(),
   ],
-  
   base: `/${repoName}/`, 
-})
+});
