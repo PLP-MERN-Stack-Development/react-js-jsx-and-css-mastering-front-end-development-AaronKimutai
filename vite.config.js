c217fb9711fcd77dcda5d71251523301d8963cf5
AaronKimutai <aaronkimutai24@gmail.com>
2025-10-23 01:10:16 +0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-const repoName = 'react-js-jsx-and-css-masterinf-front-end-development';
+const repoName = 'react-js-jsx-and-css-mastering-front-end-development';
 
 export default defineConfig({
   plugins: [
